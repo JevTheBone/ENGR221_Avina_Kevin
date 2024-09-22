98d7e1f3aa6b15bfdbca694099b99aa7d87a814b
JevTheBone <kevinavina12@hotmail.com>
2024-09-22 14:07:56 -0700

    Lab 3 Commit

--- Lab3/sortingFunctions.py
+++ Lab3/sortingFunctions.py
@@ -9,18 +9,38 @@ import time, random
 # Implementation of insertionSort algorithm
 def insertionSort(list_to_sort:list) -> list:
     ### Add your insertionSort implementation here
+    # Check through the number of indexes in list to sort
     for i in range(len(list_to_sort)):
+        # Assign index 'i' to value 'j'
         j = i
-        while j > 0 and list_to_sort[j] > list_to_sort[j-1]:
+        # Check if 'j-1' is grater than the value at 'j' for the list
+        while j > 0 and list_to_sort[j-1] > list_to_sort[j]:
+            ## Swap values between [j] and [j-1]
+            # Store list[j] value
             temp = list_to_sort[j]
+            # Assign list to sort[j] the value at [j-1]
             list_to_sort[j] = list_to_sort[j-1]
-            j -= 1
-            list_to_sort[j] = temp
+            # Assign list to sort[j-1] the original value of list to sort[j] using temp.
+            list_to_sort[j-1] = temp
+            # Update 'j' value index with 'j-1'
+            j = j-1
     return list_to_sort
 
 # Implementation of bubbleSort algorithm
 def bubbleSort(list_to_sort:list) -> list:
     ### Add your bubbleSort implementation here
+    #print(list_to_sort)
+    # Check for index is within the size of list to sort - 1
+    for i in range(len(list_to_sort) - 1):
+        # Index j within the size of list - 1 and - index 'i'
+        for j in range(len(list_to_sort) - 1 - i):
+            # If list[j] is grater than list[j+1]
+            if list_to_sort[j] > list_to_sort[j+1]:
+                # Swap values between [j] and [j+1]
+                temp = list_to_sort[j]
+                list_to_sort[j] = list_to_sort[j+1]
+                list_to_sort[j+1] = temp
+
     return list_to_sort
 
 # Returns a random list of the given length
