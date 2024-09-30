f5ef9aef72a09481a3856aa6fdd0784dba5a4e9f
JevTheBone <kevinavina12@hotmail.com>
2024-09-30 13:16:34 -0700

    Lab5 starter code

---
+++ Lab5/deque.py
@@ -0,0 +1,39 @@
+"""
+WRITE YOUR PROGRAM HEADER HERE
+"""
+
+from .doublyLinkedList import DoublyLinkedList
+
+class Deque():
+    def __init__(self):
+        self.__values = DoublyLinkedList()
+
+    def isEmpty(self):
+        pass
+    
+    def __len__(self):
+        pass
+    
+    def __str__(self):
+        pass
+
+    def peekLeft(self):
+        pass
+
+    def peekRight(self):
+        pass
+
+    def insertLeft(self, value):
+        pass
+        
+    def insertRight(self, value): 
+        pass
+
+    def removeLeft(self): 
+        pass
+
+    def removeRight(self):
+        pass
+    
+if __name__ == "__main__":
+    pass
