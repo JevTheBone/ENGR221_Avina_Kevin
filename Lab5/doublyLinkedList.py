7715d5508b5515e6c63de38fb9fb2bb2aff74baf
JevTheBone <kevinavina12@hotmail.com>
2024-10-09 15:06:32 -0700

    Lab5 Part3 updating implemented methods

--- Lab5/doublyLinkedList.py
+++ Lab5/doublyLinkedList.py
@@ -16,27 +16,49 @@ class DoublyLinkedList():
 
     # Determine whether or not the list is empty
     def isEmpty(self) -> bool:
-        if self.getFirstNode().__nextNode == None and self.getLastNode().__previousNode == None:
-            return True
+       return self.getFirstNode() == None
 
-    
+    # Set the first node of the list to a new node 
     def first(self):
-        pass
+        if self.isEmpty():
+            raise Exception("Error: List is empty, no value to return.")
+        return self.getFirstNode().getValue()
     
+    # Return the first node in the list
     def getFirstNode(self) -> DoubleNode:
         return self.__firstNode
 
+    # Return the last node in the list
     def getLastNode(self) -> DoubleNode:
         return self.__lastNode
     
+    # Set the first node of the list to a new node
     def setFirstNode(self, node):
-        pass
+        # Raise an exception if the input is not a valid node
+        if node != None and type(node) != DoubleNode:
+            raise Exception("Error: Input must be valid DoubleNode or None")
+        else: 
+            self.__firstNode = node
 
+    # Set the first node of the list to a new node   
     def setLastNode(self, node):
-        pass
+        # Raise an exception if the input is not a valid node
+        if node != None and type(node) != DoubleNode:
+            raise Exception("Error: Input must be valid DoubleNode or None")
+        else: 
+            self.__lastNode = node
 
     def find(self, value):
-        pass
+        # Traverse down the list, starting with the first node
+        node = self.getFirstNode()
+        while node != None:
+            # If this node has the given value, return it
+            if node.getValue() == value:
+                return node 
+            # Otherwise, grab the next node to check
+            node = node.getNextNode()
+        # If the value was not found, return None
+        return None
 
     def insertFront(self, value):
         pass
